1a6e39e7a36447961b0e513b0b58fe4d54b9fcb6
flixjavier <flixjavier@gmail.com>
2024-06-17 21:40:48 -0600

    Project ended

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,24 @@
-const currentYear = document.querySelector("#current-year"); 
-const lastModified =  document.querySelector("#lastModified"); 
-
 // use the date object
 const today = new Date();
 
-lastModified.innerHTML = <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`:
+const options = {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false // 24-hour format
+};
+
+const formattedDate = new Intl.DateTimeFormat('en-US', options).format(today);
+
+const year = document.querySelector("#current-year");
+
+const lastModified = document.querySelector("#lastModified");
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `<span class="highlight">Last modification: ${formattedDate}</span>`; 
+
 
